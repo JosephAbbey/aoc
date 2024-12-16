d2050eb953f387e5abe5c15e1f7b024e1ea12d63
Joseph Abbey <me@josephabbey.dev>
2024-12-16 18:03:17 +0000

    2015/08

--- 2015/08.b.py
+++ 2015/08.b.py
@@ -1,3 +1,14 @@
 with open("2015/08.input.txt", encoding="utf-8") as file:
     data = file.read()
 
+lines = data.splitlines()
+lines = filter(lambda x: len(x) > 0, lines)
+
+total = 0
+for line in lines:
+    total += 2
+    for char in line:
+        if char in ["\\", '"']:
+            total += 1
+
+print(total)
