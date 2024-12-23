f6c6d38a4bc14902d3af507c14c085f16d5eac7b
Joseph Abbey <me@josephabbey.dev>
2024-12-23 15:00:23 +0000

    2015/12 Part 1

--- 2015/12.a.py
+++ 2015/12.a.py
@@ -1,3 +1,16 @@
 with open("2015/12.input.txt", encoding="utf-8") as file:
     data = file.read()
 
+json = data.strip()
+
+total = 0
+num = ""
+for c in json:
+    if c.isdigit() or c == "-":
+        num += c
+    else:
+        if num:
+            total += int(num)
+            num = ""
+
+print(total)
